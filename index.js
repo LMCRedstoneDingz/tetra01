9ce106aaa7aa4d3dfac96d5e5b0551db2da56b1c
lmcredstonedingz <landree.allen04@gmail.com>
2017-03-13 19:16:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(process.env.PORT, function() {
-  console.log("Running! "+process.env.PORT);
+app.use(function(req, res) {
+   res.send('404: Page not Found', 404);
+});
+  
+app.use(function(error, req, res, next) {
+   res.send('500: Internal Server Error', 500);
+});
+app.listen(process.env.PORT,()=>{
+  console.log("Running!");
 });
