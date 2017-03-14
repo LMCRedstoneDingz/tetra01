014a5b18a30c69263601e7c4fa0998d0d474c256
lmcredstonedingz <landree.allen04@gmail.com>
2017-03-13 20:59:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var express = require("express");
-var app = express();
-var http = require("http").Server(app);
+const express = require("express");
+const http = require("http");
 var WebSocket = require("ws");
+
+var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.use(function(req, res) { 
+app.use((req, res)=>{ 
   res.send("404: File not found", 404);
 });
 
-app.use(function(err, req, res, nxt) {
+app.use((err, req, res, nxt)=>{
   res.send("500: Interal Server Error", 500);
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection',(ws)=>{ 
+  ws.on('message',(msg)=>{
+    console.log("Received: "+msg);
+  });
+ 
+  ws.send("something");
 });
  
 http.listen(process.env.PORT,()=>{
   console.log("Running!");
 });
-
-const wss = new WebSocket.Server(http);
-
-wss.on('connection', (ws) => {
-  ws.send("Hi");
-});
-
-/*setInterval(()=>{
-  wss.clients.forEach((client)=>{
-    client.send(new Date().toTimeString());
-  });
-}, 1000);*/
