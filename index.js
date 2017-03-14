6c92ccbef9ba899ec95e01a95e81d30c88808480
lmcredstonedingz <landree.allen04@gmail.com>
2017-03-13 20:08:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/port",function(req, res) {
+  res.send(process.env.PORT);
+});
+
 app.use(function(req, res) {
-   res.status(404);
-   res.sendFile(__dirname + "/err/404.html");
+  res.status(404);
+  res.sendFile(__dirname + "/err/404.html");
 });
 
 app.use(function(err, req, res, nxt) {
-   res.status(500);
-   res.sendFile(__dirname + "/err/500.html");
+  res.status(500);
+  res.sendFile(__dirname + "/err/500.html");
 });
 
 app.listen(process.env.PORT,()=>{
